7c1131fa38f9a53cdced37b551a0e0cfcd589164
Anton Myerkin <anton.mr@2muchcoffee.com>
2019-11-26 10:48:59 +0200

    feat(editor) add empty editor component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { EditorModule } from './editor/editor.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    BrowserModule,
     NgbModule,
-    BrowserModule
+    EditorModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/editor/editor.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+
+import { EditorComponent } from './editor.component';
+
+@NgModule({
+  declarations: [
+    EditorComponent
+  ],
+  imports: [
+  ],
+  exports: [EditorComponent]
+})
+export class EditorModule { }
