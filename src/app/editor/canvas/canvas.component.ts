6d4dda17602a77c21f031ccaffa0119298c71f4d
Anton Myerkin <anton.mr@2muchcoffee.com>
2019-11-26 10:49:08 +0200

    feat(editor:canvas) added simple text and image

--- src/app/editor/canvas/canvas.component.ts
+++ src/app/editor/canvas/canvas.component.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-canvas',
   templateUrl: './canvas.component.html',
   styleUrls: ['./canvas.component.sass']
 })
-export class CanvasComponent {
+export class CanvasComponent implements AfterViewInit {
+  canvasHeight = 700;
+  canvasWidth = 500;
+
+  @ViewChild('canvasElement', {static: false})
+  private canvas: ElementRef;
+  private canvasElement: HTMLCanvasElement;
+  private ctx: CanvasRenderingContext2D;
+
+  ngAfterViewInit(): void {
+    this.canvasElement = this.canvas.nativeElement;
+    this.ctx = this.canvasElement.getContext('2d');
+    this.ctx.globalCompositeOperation = 'destination-over';
+
+    this.setImage();
+    this.setText();
+  }
+
+  setText() {
+    this.ctx.font = '30px Arial';
+    this.ctx.fillText('Hello World', 10, 50);
+  }
+
+  setImage() {
+    const background = new Image();
+    background.src = 'http://placehold.it/500x700';
+    background.onload = () => {
+      this.ctx.drawImage(background, 0, 0);
+    };
+  }
 }
